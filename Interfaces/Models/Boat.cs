b1e7aba1d4eac19ccd7fe78fabd8a75d7bf8f69d
Apostolos <apostoak@gmail.com>
2020-11-05 21:39:28 +0200

    Add project files.

---
+++ Interfaces/Models/Boat.cs
@@ -0,0 +1,45 @@
+﻿using Interfaces.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Models
+{
+    class Boat : AutoMobile, IAutoMobileActions, IBoatActions
+    {
+
+        public Boat()
+        {
+
+        }
+
+        public Boat(bool IsOn)
+        {
+            this.IsOn = IsOn;
+        }
+
+        public void SteerLeft()
+        {
+            Console.WriteLine("The boat is steering left");
+            this.Steering = true;
+        }
+
+        public void Reverse()
+        {
+            Console.WriteLine("The boat is reversing");
+        }
+
+        public void SteerRight()
+        {
+            Console.WriteLine("The boat is steering right");
+            this.Steering = false;
+        }
+
+        public void Steer(bool steerLeft)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,78 @@
+﻿using Interfaces.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // NO NO can't have instances of an abstract class or interface
+            //AutoMobile auto = new AutoMobile();
+
+            //Console.WriteLine("car c");
+            //Car c = new Car(true);
+            //c.SteerLeft();
+
+            Console.WriteLine("car");
+            Car car = new Car();
+            Console.WriteLine(car.IsOn);
+            car.Start();
+            car.SteerLeft();
+            Console.WriteLine(car.Steering);
+            Console.WriteLine(car.Steering);
+            Console.WriteLine(car.Steering);
+            car.SteerRight();
+            Console.WriteLine(car.Steering);
+            Console.WriteLine(car.Steering);
+            car.SteerLeft();
+            Console.WriteLine(car.Steering);
+            Console.WriteLine(car.Steering);
+            Console.WriteLine(car.IsOn);
+            
+            car.Stop();
+            Console.WriteLine(car.IsOn);
+
+            // with private set doesn't work
+            //car.IsOn = false;
+
+
+
+            Console.WriteLine("motorbike");
+            MotorBike moto = new MotorBike();
+            moto.Start();
+            moto.SteerLeft();
+            moto.SteerRight();
+            moto.Stop();
+            Console.WriteLine(moto.IsOn);
+
+            //Console.WriteLine("motorbike moto2");
+            //MotorBike moto2 = new MotorBike(true);
+            //Console.WriteLine(moto2.IsOn);
+            //moto2.SteerLeft();
+            //moto2.Stop();
+            //Console.WriteLine(moto2.IsOn);
+            Console.WriteLine("Boat");
+            Boat boat = new Boat();
+            Console.WriteLine(boat.IsOn);
+            boat.Start();
+            boat.SteerLeft();
+            boat.SteerRight();
+            boat.Stop();
+            Console.WriteLine(boat.IsOn);
+
+            
+            
+            
+            
+            
+            
+            Console.ReadLine();
+
+        }
+    }
+}
